0a84c3e5f56d62989f248f0650b1412a0a40282e
asdzxc727 <64668379+asdzxc727@users.noreply.github.com>
2023-01-17 11:24:27 -0800

    start app
    create app

---
+++ lights_out/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Cell from './Components/Cell';
+
+function App() {
+  return (
+    <div className="App">
+      <Cell />
+    </div>
+  );
+}
+
+export default App;
